508db8fbc73204f3af2e69ff30be41441f62a877
Serhii Puchkovskyi <serhiipuchkovskii@gmail.com>
2020-02-26 15:29:19 +0200

    Add applyMiddleware function.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Simple Redux
 Working implementation of Redux features in a simple way. For those, who prefer to understand the tools that you're using.
 
+> **NOTE:** Naïve implementation! That's not Redux API.
+
 ### Refs
 https://egghead.io/courses/getting-started-with-redux
---
+++ applyMiddleware.js
@@ -0,0 +1,9 @@
+const applyMiddleware = (store, middlewares) => {
+  middlewares = middlewares.slice()
+  middlewares.reverse()
+
+  let dispatch = store.dispatch
+  middlewares.forEach(middleware => (dispatch = middleware(store)(dispatch)))
+
+  return Object.assign({}, store, { dispatch })
+}
